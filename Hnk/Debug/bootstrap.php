6830f92ad11f3947c70f2a5951ac9604b1a1b06c
pgdba <pgdba@gmail.com>
2015-03-31 10:45:24 +0200

    added debug modes

--- Hnk/Debug/bootstrap.php
+++ Hnk/Debug/bootstrap.php
@@ -4,4 +4,9 @@ define('APP_DIR', __DIR__);
 define('BASE_DIR', dirname(dirname(APP_DIR)));
 
 require_once APP_DIR . '/autoload.php';
+require_once APP_DIR . '/helpers.php';
+
+if (!defined('HNK_DEBUG_MODE')) {
+    define('HNK_DEBUG_MODE', \Hnk\Debug\Config\ConfigInterface::MODE_OFF);
+}
 
